38444e73edde27fdfce0f1cb926d225e7fd5b6cb
ukupode <ukupode@greeny.cs.tlu.ee>
2017-02-27 13:01:49 +0200

    ylesanne 2

--- Kronoloogiline.java
+++ Kronoloogiline.java
@@ -5,6 +5,7 @@ import java.net.*;
 public class Kronoloogiline{
 	String failiAsukoht;
 	double keskmineKolmest=0;
+	double ridadeArv=0;
 	public Kronoloogiline(String failiAsukoht){
 		this.failiAsukoht = failiAsukoht;
 	}
@@ -37,6 +38,56 @@ public class Kronoloogiline{
 		}
 		return keskmineKolmest;
 	}
-	
-	
+
+	public double ylesanne2(int tulbanr) throws IOException{
+		
+		BufferedReader lugeja=kysiLugeja();
+		boolean alustatud=false;
+		String rida=lugeja.readLine();
+		rida=lugeja.readLine();
+		int kogus=0;
+		int ridu=0;
+		double esimene=0;
+		double viimane=0;
+		double koik=0;
+		while(rida!=null){
+		String[] m=rida.split(" ");
+			try{
+				if (ridu==0){
+					esimene=Double.parseDouble(m[tulbanr])/2;
+					kogus++;
+					ridu++;
+				}else{
+					koik = koik+(Double.parseDouble(m[tulbanr]));
+					kogus++;
+					ridu++;
+					
+				}
+			viimane=Double.parseDouble(m[tulbanr])/2;
+			} catch(Exception veaandmed){
+				 System.out.println("Viga.");
+			}
+			rida=lugeja.readLine();
+		}
+		System.out.println("kogus "+kogus);
+		System.out.println("ridu "+ridu);
+		System.out.println("koik "+koik);
+		System.out.println("esimene "+esimene);
+		System.out.println("viimane "+viimane);
+		double keskmine=((esimene+viimane+koik)/kogus);
+		lugeja.close();
+		return keskmine;
+		
+/*
+		
+		[ukupode@greeny t04kontrolltoo_I]$ java Proov
+		kogus 7
+		ridu 7
+		koik 38.67
+		esimene 0.89
+		viimane 1.77
+		Keskmine on : 5.904285714285714
+*/
+	}
 }
+	
--- Proov.java
+++ Proov.java
@@ -1,10 +1,13 @@
 import java.io.*;
 public class Proov{
 	public static void main(String[] args) throws IOException{
-		Kronoloogiline krono= new Kronoloogiline(1,1,1);
-		System.out.println(krono.ylesanne1(10 , 10 , 10));
-		System.out.println(krono.ylesanne1(0 , -10 , 0));
-		System.out.println(krono.ylesanne1(7 , 122 , 5));
-		System.out.println(krono.ylesanne1(14 , 0 , 1));
+		//Kronoloogiline krono1= new Kronoloogiline(1,1,1);
+		//System.out.println(krono1.ylesanne1(10 , 10 , 10));
+		//System.out.println(krono1.ylesanne1(0 , -10 , 0));
+		//System.out.println(krono1.ylesanne1(7 , 122 , 5));
+		//System.out.println(krono1.ylesanne1(14 , 0 , 1));
+		Kronoloogiline krono2 = new Kronoloogiline("arvud.txt");
+		System.out.println("Keskmine on : "+krono2.ylesanne2(1));
+		
 	}
 }
